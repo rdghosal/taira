b701595c5a925524de5b6ba6944b4a53ad0ea7f5
Rahul Ghosal <rdghosal@gmail.com>
2023-04-08 18:05:14 -0400

    feat: map Value from parsed_json to HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::fs;
+
 // use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use serde_json::Value;
+
+// #![feature(type_name_of_val)]
+// use std::any::type_name_of_val;
 
 
 // #[derive(Serialize, Deserialize)]
@@ -8,12 +13,15 @@ use serde_json::Value;
 //     test: i32,
 // }
 
-fn parse_json(filename: &str) -> Value {
+fn parse_json(filename: &str) -> HashMap<String, Value> {
     let data = fs::read_to_string(filename).expect("failed to read file");
     serde_json::from_str(&data).expect("Failed to read json file: {filename}")
 }
 
 fn main() {
     // let t: TestStruct = serde_json::from_str(&data).expect("failed to parse json data");
-    println!("{}", parse_json("./test.json"));
+    let parsed = parse_json("./test.json");
+    for (key, _) in parsed.iter() {
+        println!("{}", key)
+    }
 }
