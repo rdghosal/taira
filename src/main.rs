afcdc308d8b6a751431247dfd5d989ff5c56aa7c
Rahul Ghosal <rdghosal@gmail.com>
2023-04-09 09:21:50 -0400

    WIP refactor parse_json to json_to_map and parse_json_map

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 // use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 // #![feature(type_name_of_val)]
 // use std::any::type_name_of_val;
@@ -13,24 +13,36 @@ use serde_json::Value;
 //     test: i32,
 // }
 
-fn parse_json(filename: &str) -> HashMap<String, Value> {
+fn json_to_map(filename: &str) -> Map<String, Value> {
     let data = fs::read_to_string(filename).expect("failed to read file");
     serde_json::from_str(&data).expect("Failed to read json file: {filename}")
 }
 
+fn parse_json_map(parent: &mut Map<String, Value>, current_idx: Option<usize>) {
+    for (k, v) in parent.iter() {
+        if v.is_object() {
+            let mut next: Map<String, Value> = v.as_object().unwrap().clone();
+            parse_json_map(&mut next, None);
+        }
+        else if v.is_array() {
+            for (i, element) in v.as_array().unwrap().iter().enumerate() {
+                let mut next: Map<String, Value> = element.as_object().expect("not an obj!").clone();
+                parse_json_map(&mut next, Some(i));
+            }
+        }
+        else {
+            continue;
+        }
+    }
+}
+
+
+
+
 fn main() {
     // let t: TestStruct = serde_json::from_str(&data).expect("failed to parse json data");
-    let parsed = parse_json("./test.json");
-    let mut keys: Vec<_> = parsed.keys().clone().collect();
-    for (_, value) in parsed.iter() {
-        if value.is_object() {
-            // println!("{:?}", value);
-        }
-        else if value.is_array() {
-            // println!("{:?}", value);
-        }
-    }
-    for k in keys.iter() {
-        println!("{}", &k);
-    }
+    //
+    let mut map = json_to_map("./test.json");
+    parse_json_map(&mut map, None);
+
 }
