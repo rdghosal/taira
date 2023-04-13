cc0eeda2b6c636e09c7020cb754db078c85ec2ae
Rahul Ghosal <rdghosal@gmail.com>
2023-04-12 22:48:03 -0400

    feat: fix return for parse_json_map and add key prefixes, fix mutation issues

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::{fs, borrow::BorrowMut};
+use std::{borrow::BorrowMut, fs};
 
 // use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use serde_json::{Map, Value};
+use std::collections::HashMap;
 
 // #![feature(type_name_of_val)]
 // use std::any::type_name_of_val;
-
 
 // #[derive(Serialize, Deserialize)]
 // struct TestStruct {
@@ -20,38 +19,42 @@ fn json_to_map(filename: &str) -> Map<String, Value> {
 
 fn parse_json_map(
     current: &mut Map<String, Value>,
-    parent: &mut Option<Map<String, Value>>, 
-    current_idx: Option<usize>) -> Map<String, Value> {
-
+    parent: &mut Option<&mut Map<String, Value>>,
+    prefix: Option<&str>,
+) {
     for (k, v) in current.clone().iter_mut() {
         match v {
-            Value::Object(next) => { parse_json_map(next, &mut Some(current.to_owned()), None); }
+            Value::Object(next) => {
+                parse_json_map(next, &mut Some(current), Some(&k.to_string()));
+                current.remove(k);
+            }
             Value::Array(array) => {
                 for (i, element) in array.iter_mut().enumerate() {
                     if let Value::Object(next) = element {
-                        parse_json_map(next, &mut Some(current.to_owned()), Some(i));
+                        parse_json_map(next, &mut Some(current), Some(&i.to_string()));
+                        current.remove(k);
                     } else {
                         println!("error!");
                     }
-                };
-            },
+                }
+            }
             // todo
             _ => {
                 if let Option::Some(p) = parent {
-                    p.insert(k.to_string(), v.clone());
+                    println!("inserting {}: {}", k, v);
+                    p.insert(format!("{}_{}", prefix.unwrap(), k.to_string()), v.clone());
                 }
             }
         }
     }
-
-    current.to_owned()
 }
 
 fn main() {
     // let t: TestStruct = serde_json::from_str(&data).expect("failed to parse json data");
     let mut map = json_to_map("./test.json");
-    map = parse_json_map(&mut map, &mut None, None);
+    parse_json_map(&mut map, &mut None, None);
     for k in map.keys() {
         println!("{}", k);
+        println!("{}", map[k]);
     }
 }
